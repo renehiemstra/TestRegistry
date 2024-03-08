0d6c040b0aab08af062b9e038657852fa407aa8b
rrhiemstar <rrhiemstar@gmail.com>
2024-03-08 11:34:23 +0100

    Initialized new registry.

---
+++ Registry.lua
@@ -0,0 +1,9 @@
+Registry = {
+    name = "TestRegistry",
+    uuid = "0b01956a-4888-4e0d-8fb7-5440c266ef52",
+    url  = "git@github.com:renehiemstra/TestRegistry",
+    description = "Cosm local package registry",
+    packages = {
+    },
+}
+return Registry
