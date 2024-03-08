2067a6136dcd45d0c13ad610fdffb65b5147e695
rrhiemstar <rrhiemstar@gmail.com>
2024-03-08 11:35:29 +0100

    <new package> DepDep

---
+++ D/DepDep/1.0.0/Specs.lua
@@ -0,0 +1,10 @@
+Project = {
+    name = "DepDep",
+    uuid = "26609f9c-bf86-4ba2-86db-c927209938d9",
+    version = "1.0.0",
+    url  = "git@github.com:renehiemstra/DepDep",
+    sha1 = "ae2babf0797be4a180bf97eacdfc273938642aa7",
+    deps = {
+    },
+}
+return Project
